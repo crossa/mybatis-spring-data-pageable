f71b0fdd5020f79ce1f6aa651f69fafc014f1945
crossa <421395590@qq.com>
2023-04-24 22:03:42 +0800

    Initial commit
    1. 移除自动添加limit的操作

--- src/main/java/com/github/crossa/MybatisSpringDataPageableInterceptor.java
+++ src/main/java/com/github/crossa/MybatisSpringDataPageableInterceptor.java
@@ -11,9 +11,11 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+
 import java.sql.Connection;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Optional;
 
 @Intercepts({@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class})})
 public class MybatisSpringDataPageableInterceptor implements Interceptor {
@@ -25,37 +27,40 @@ public class MybatisSpringDataPageableInterceptor implements Interceptor {
     private static final String SELECT = "SELECT";
     private static final String LIMIT_U = "LIMIT ";
     private static final String LIMIT_L = "limit ";
+
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         StatementHandler handler = (StatementHandler) invocation.getTarget();
         MetaObject metaObject = SystemMetaObject.forObject(handler);
         MappedStatement mappedStatement = (MappedStatement) metaObject.getValue(MAPPED_STATEMENT);
-        String sql = handler.getBoundSql().getSql();
-        if (SELECT.equals(mappedStatement.getSqlCommandType().name()) && !sql.contains(LIMIT_U)
-                && !sql.contains(LIMIT_L)) {
+        String sqlStr = handler.getBoundSql().getSql().replaceAll("\n", "").replaceAll("  ", " ");
+        StringBuffer sql = new StringBuffer(handler.getBoundSql().getSql());
+        if (SELECT.equals(mappedStatement.getSqlCommandType().name()) && !sqlStr.contains(LIMIT_U)
+                && !sqlStr.contains(LIMIT_L)) {
             ParameterHandler parameterHandler = (ParameterHandler) metaObject.getValue(PARAMETER_HANDLER);
             Object parameter = parameterHandler.getParameterObject();
-            if(parameter instanceof Map) {
+            if (parameter instanceof Map) {
                 Map<String, Object> params = (Map<String, Object>) parameter;
-                Pageable pageable = searchPageable(params);
-                sql += " limit " + (pageable.getOffset()) + "," + pageable.getPageSize();
+                searchPageable(params).ifPresent(pageable -> {
+                    sql.append(" limit " + (pageable.getOffset()) + "," + pageable.getPageSize());
+                });
             }
-            metaObject.setValue(BOUND_SQL, sql);
+            metaObject.setValue(BOUND_SQL, sql.toString());
         }
         return invocation.proceed();
     }
 
-    public Pageable searchPageable(Map<String, Object> params) {
+    public Optional<Pageable> searchPageable(Map<String, Object> params) {
         if (params.containsKey(PAGEABLE) && params.get(PAGEABLE) instanceof Pageable) {
-            return (Pageable) params.get(PAGEABLE);
+            return Optional.ofNullable((Pageable) params.get(PAGEABLE));
         }
         Iterator<String> key = params.keySet().iterator();
         while (key.hasNext()) {
             Object obj = params.get(key.next());
             if (obj instanceof Pageable) {
-                return (Pageable) obj;
+                return Optional.ofNullable((Pageable) obj);
             }
         }
-        return PageRequest.of(0, 1);
+        return Optional.empty();
     }
 }
